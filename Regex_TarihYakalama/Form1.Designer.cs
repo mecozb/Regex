fc5f6bc5b7082244bdcd5c2747c15661372d00f2
Administrator <Administrator@TGM-000>
2018-09-10 14:08:20 +0300

    Add project files.

---
+++ Regex_TarihYakalama/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Regex_TarihYakalama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 280);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "GG-AA-YYYY";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tarih :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(85, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tel No :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(130, 97);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "05XX-XXX-XX-XX";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "TCKN :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(130, 138);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(69, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Password :";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(130, 177);
+            this.textBox4.MaxLength = 14;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.PasswordChar = '*';
+            this.textBox4.Size = new System.Drawing.Size(140, 20);
+            this.textBox4.TabIndex = 8;
+            this.textBox4.TextChanged += new System.EventHandler(this.password_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.LightGray;
+            this.progressBar1.Location = new System.Drawing.Point(276, 180);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 13);
+            this.progressBar1.Step = 5;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(382, 180);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Zayıf";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 513);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Regex_TarihYakalama/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Regex_TarihYakalama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            progressBar1.Maximum = 5;
+            progressBar1.Value = 0;
+           // Application.EnableVisualStyles();
+            progressBar1.ForeColor = Color.Green;
+        }
+
+        Regex sLetter = new Regex(@"[a-z]");
+        Regex uLetter = new Regex(@"[A-Z]");
+        Regex number = new Regex(@"[0-9]");
+        Regex signs = new Regex(@"[\.]|[\?]|[\\]|[\*]|[\;]|[\,]|[\:]|[-_!']");
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Regular Expression için bir desen (pattern) tanımlıyoruz :
+
+            string tarihDeseni = @"((0[1-9])|([12][0-9])|(3[01]))(-)((0?[1-9])|(1[0-2]))(-)([12][0-9][0-9][0-9])";
+
+            string telefonDeseni = @"(0?5[0-9][0-9])(-)([0-9][0-9][0-9])(-)([0-9][0-9])(-)([0-9][0-9])";
+
+            string TCKN_Deseni = @"[1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][02468]";
+
+
+            // Regular Expressionumuzu tanımlıyoruz :
+           
+
+            Regex regexTarih = new Regex(tarihDeseni);
+            Regex regexTelefon = new Regex(telefonDeseni);
+            Regex regexTCKN = new Regex(TCKN_Deseni);
+            Regex regexPassword = new Regex(@"([a-zA-Z0-9p{P}P{L}]{8,14})");
+            // Tarih arayacağımız metni konsoldan alıyoruz :
+
+            string info = "Tarih: " + textBox1.Text + "\n" + "Tel No: " + textBox2.Text + "\n" + "TCKN: " + textBox3.Text;
+
+            // Metin içerisindeki tarihleri (birden fazla olabilir) Collection nesnesine atıyoruz :
+
+            if(regexTarih.IsMatch(textBox1.Text) && regexTelefon.IsMatch(textBox2.Text) && regexTCKN.IsMatch(textBox3.Text) )
+            {
+                if (regexPassword.IsMatch(textBox4.Text))
+                    MessageBox.Show(info);
+                else
+                    MessageBox.Show("Parola geçersiz karakterler içeriyor.");
+
+            }
+            else
+            {
+                MessageBox.Show("Tarih formatı,telefon formatı veya TCKN hatalı.Lütfen 'GG-AA-YYYY' '05XX-XXX-XX-XX' şeklinde deneyiniz.");
+            }
+
+
+        }
+
+        private void password_TextChanged(object sender, EventArgs e)
+        {
+            int count = 0;
+            if (sLetter.IsMatch(textBox4.Text)) { count++; }
+            if (uLetter.IsMatch(textBox4.Text)) { count++; }
+            if (number.IsMatch(textBox4.Text)) { count++; }
+            if (signs.IsMatch(textBox4.Text)) { count+=2; }
+
+            if(count < 3)
+            {
+                label5.Text = "Zayıf";
+                progressBar1.ForeColor = Color.Red;
+                progressBar1.Value = count;
+            }
+            else if (count == 3)
+            {
+                label5.Text = "Orta";
+                progressBar1.ForeColor = Color.Yellow;
+                progressBar1.Value = count;
+            }
+            else if (count > 3)
+            {
+                label5.Text = "Güçlü";
+                progressBar1.ForeColor = Color.Green;
+                progressBar1.Value = count;
+            }
+
+        }
+    }
+}
